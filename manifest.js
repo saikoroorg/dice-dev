772a0665b15d3eca0fee1d2d17802fe21f6cd929
@ <@>
2021-09-15 18:17:39 +0900

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Dice",
-    "version": "0.8.10914b",
+    "version": "0.8.10915",
     "short_name": "Dice",
     "background_color": "#fff",
     "theme_color": "#fff",
